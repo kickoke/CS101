96e65c1ae38cf52b03dc8093d9c3c886278770fd
Tina Luedtke <tina@kickoke.com>
2026-03-30 20:10:53 -0700

    Update quicksort.py

--- practice/1-1/quicksort.py
+++ practice/1-1/quicksort.py
@@ -10,3 +10,36 @@ Approach:
 
 to_sort_unique = "asdfghjklzxcvbnm"
 to_sort_not_unique = "asdfghjklzxcvghjki"
+
+def quicksort(to_sort: str) -> list:
+  # Base case: already sorted
+  if len(to_sort) == 1:
+    to_sort = sorted
+    return sorted
+  
+  pivot_index = int(len(to_sort)/2)
+  pivot_value = to_sort[pivot_index]
+  to_sort_left = to_sort[:pivot_index]
+  to_sort_right = to_sort[pivot_index+1:]
+  left =[]
+  right =[]
+
+  while to_sort_left:
+    if 
+
+
+def is_unique(sorted):
+    for i in range(0, (len(sorted)-1)):
+        if sorted[i] == sorted[i+1]:
+            return False
+    return True
+
+
+sorted_unique = quicksort(to_sort_unique)
+
+print(sorted_unique)
+print(is_unique(sorted_unique))
+
+sorted_not_unique = quicksort(to_sort_not_unique)
+print(sorted_not_unique)
+print(is_unique(sorted_not_unique))
