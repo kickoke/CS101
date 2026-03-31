1bb1fd689a44c7846429ab2e4aa936fcfba33fcb
Tina Luedtke <tina@kickoke.com>
2026-03-30 20:02:14 -0700

    Update quicksort.py

--- practice/1-1/quicksort.py
+++ practice/1-1/quicksort.py
@@ -1,1 +1,12 @@
+"""
+Practice Question 1.1
 
+How to check if a string has only unique values?
+
+Approach:
+1. Sort string (Merge sort)
+2. Check if two neighboring values are the same
+"""
+
+to_sort_unique = "asdfghjklzxcvbnm"
+to_sort_not_unique = "asdfghjklzxcvghjki"
